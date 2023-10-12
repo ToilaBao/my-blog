d1063be59e715e9b504653626fafc61c22c25259
ToilaBao <thienbaodx2000@gmail.com>
2023-10-12 09:40:42 +0700

    front-end

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBArQjAlbl0-nA4BFUb6MUBWgVX4dQXo4I",
+  authDomain: "my-react-blog-1b40d.firebaseapp.com",
+  projectId: "my-react-blog-1b40d",
+  storageBucket: "my-react-blog-1b40d.appspot.com",
+  messagingSenderId: "774263345447",
+  appId: "1:774263345447:web:0eb22e605ab5dfe8049902",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
